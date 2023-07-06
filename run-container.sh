ba79e27d615b343ee288cc34af3e13772b6fbb3b
Michael Mayer <michael.mayer@posit.co>
2023-07-06 13:50:58 +0000

    various fixes

--- run-container.sh
+++ run-container.sh
@@ -4,7 +4,7 @@ source global.sh
 
 EASYBUILD_SOURCE="/opt/apps/easybuild/sources"
 EB_REPOS_PATH="${PWD}/ebfiles_repo"
-BUILD_OS="jammy"
+BUILD_OS="focal"
 EASYBUILD_PATH="/opt/apps/easybuild-${BUILD_OS}"
 SCRATCH_PATH="/scratch/eb/${BUILD_OS}"
 
@@ -13,4 +13,4 @@ mkdir -p ${EASYBUILD_PATH} ${SCRATCH_PATH}
 singularity exec \
 	--bind ${SCRATCH_PATH}:/scratch:rw,${EASYBUILD_PATH}:/opt/apps/easybuild,${EB_REPOS_PATH}:/ebfiles_repo,${EASYBUILD_SOURCE}:/opt/apps/easybuild/sources \
 	${CONTAINER_PATH}/builder-${BUILD_OS}.simg \
-        eb --quiet -f Python-3.10.4-GCCcore-11.3.0.eb R-4.0.5-foss-2022a.eb R-4.1.2-foss-2022a.eb R-4.2.1-foss-2022a.eb	
+	eb --quiet R-bundle-Bioconductor-3.15-foss-2022a-R-4.2.1.eb
